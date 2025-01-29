5f313904a94eaccc872c7d86f9a4ce489a48f6b4
MialLewis <95620982+MialLewis@users.noreply.github.com>
2025-01-29 23:51:53 +0000

    add boost bindings api source

---
+++ api/pythonInterface/apiBindingsBoost.cpp
@@ -0,0 +1,29 @@
+#include <boost/python.hpp>
+
+class ComplexNumber {
+private:
+    double m_real;
+    double m_imaginary;
+
+public:
+    ComplexNumber() : m_real(0), m_imaginary(0) {};
+    ComplexNumber(double real, double imaginary) : m_real(real), m_imaginary(imaginary) {};
+    const double getReal() const { return m_real; }
+    const double getImaginary() const { return m_imaginary; }
+
+    ComplexNumber ComplexNumber::operator+(const ComplexNumber& rhs) { return ComplexNumber(m_real + rhs.getReal(), m_imaginary + rhs.getImaginary()); }
+};
+
+
+
+int add(int a, int b) {
+    return a + b;
+}
+
+BOOST_PYTHON_MODULE(api_boost) {
+    using namespace boost::python;
+    def("add", add);
+
+    class_<ComplexNumber>("ComplexNumber", init<double, double>())
+        .def("__add__", &ComplexNumber::operator+, (arg("left"), arg("right")));
+}
